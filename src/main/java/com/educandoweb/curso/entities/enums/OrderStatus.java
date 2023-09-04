5d3d2a6b79795e09fea4641f4abaaa53de1232cc
marciojgermano <marcio.germano@gmail.com>
2023-09-04 14:40:55 -0300

    OrderStatus enum

---
+++ src/main/java/com/educandoweb/curso/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.educandoweb.curso.entities.enums;
+
+public enum OrderStatus {
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	// Método retorna um OrderStatus se o código informado for valido.
+	public static OrderStatus valueOf(int code) {
+		for(OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
